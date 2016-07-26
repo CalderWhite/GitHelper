60841681dee34ef2290fcb0380fd51b1a767ff49
Calder White <calderwhite1@gmail.com>
2016-07-25 21:06:14 -0400

    BIG push with new functions using oauth
    you can now:
    commit files (create new ones and edit old ones)
    sign in with oauth instead of PyGithub

--- git_commands.py
+++ git_commands.py
@@ -1,13 +1,17 @@
 from github import Github
 from github import GithubException
-import sys, json, getpass, base64
+import sys, json, getpass, base64, requests, random
 import urllib.request
+import urllib.error
 
 class GitHubUser(object):
-    def __init__(self,user,pswrd):
-        self.try_write_auth(user,pswrd)
-        self.username = user
-        self.password = pswrd
+    def __init__(self,oauth_token):
+        self.oauth_token = oauth_token
+        res = urllib.request.urlopen("http://api.github.com/user?oauth_token=" + oauth_token).read()
+        j = json.loads(res.decode('utf-8'))
+        self.user_json = j
+    """
+    the PyGithun library was terrible, so I decided to just use the github v3 REST api
     def try_write_auth(self,userr,pswrd):
         global wrong_pass
         wrong_pass = False
@@ -34,34 +38,42 @@ class GitHubUser(object):
         w.write(jstr)
         w.close()
         pass
-    def commit_file(self,path,msg,file,branch,commiter,repo_url,print_status=False):
+    """
+    def commit_file(self,path,msg,file,branch,name,print_status=False):
         if print_status:
             print("retriving file sha...")
-        if repo_url[-1] != "/":
-            repo_url = repo_url + "/"
-        #print(repo_url,path,branch)
-        content_url = repo_url + "contents/" + path + "?ref=" + branch
-        sha = urllib.request.urlopen(content_url).read()
-        sha = json.loads(sha.decode('utf-8'))["sha"]
-        #print(sha)
-        jsonObj = {
-            "message" : msg,
-            "content" : file,
-            "sha" : sha,
-            "commiter" : commiter
-            }
-        jsonObj["file"] = base64.b64encode(file.encode('utf-8'))
-        jsonObj["file"] = jsonObj["file"].decode('utf-8').replace("\n","\\n")
-        strJson = json.dumps(jsonObj)
+        content_url = "http://api.github.com/repos/"+ self.user_json["login"] + "/" + name + "/contents/" + path + "?ref=" + branch + "&oauth_token=" + self.oauth_token
+        global sha
+        global no_sha
+        no_sha = False
+        try:
+            sha = urllib.request.urlopen(content_url).read()
+            sha = json.loads(sha.decode('utf-8'))["sha"]
+        except urllib.error.HTTPError:
+            e = sys.exc_info()[1]
+            if e.code == 404:
+                no_sha = True
+        # now put all the needed data into 1 string
+        # note: I didn't use .format or % () for the string since there were many Json parsing errors on the github end (400 errors)
+        # when I did. but not when I used the old fashioned way of just stringing them together.
+        email = self.user_json["email"]
+        username = self.user_json["login"]
+        file = base64.b64encode(file.encode('utf-8'))
+        file = file.decode('utf-8').replace("\n","\\n")
+        if no_sha:
+            data = '{"commiter": {"email": "' + email + '", "name": "' + username + '"}, "content": "' + file + '", "message": "' + msg + '"}'
+        else:
+            data = '{"commiter": {"email": "' + email + '", "name": "' + username + '"}, "sha": "' + sha + '", "content": "' + file + '", "message": "' + msg + '"}'
+        #print(data)
         if print_status:
             print("Making request to commit...")
-        oauth_token = None
         headers = {
-            'Authorization' : "token " + oauth_token
+            'Authorization' : "token " + self.oauth_token
             }
-        res = requests.put(content_url, headers=headers, data=strJson)
+        new_url = "https://api.github.com/repos/"+ self.user_json["login"] + "/" + name + "/contents/" + path
+        res = requests.put(new_url,data=data,headers=headers)
+        print(res.text)
         pass
-ps = getpass.getpass()
-user = GitHubUser("CalderWhite",ps)
-user.commit_file("Test.txt","commit from module","new test commit","master",{"CalderWhite","calderwhite1@gmail.com"},"https://api.github.com/repos/CalderWhite/Github_Api_Test")
-#commit_file("/Test.txt","Test commit for api.",b'Testing123',"master")
+if __name__ == '__main__':
+    user = GitHubUser(getpass.getpass("Gimme dat hash:"))
+    user.commit_file("Testey.txt","commit from module",str(random.random()),"master","Github_Api_Test",print_status=True)
